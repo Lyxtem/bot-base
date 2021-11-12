d5d25ca85975e710884a8871e903e2ec1993e2bc
Syntax <93158134+dlh-Syntax@users.noreply.github.com>
2021-11-12 17:33:23 -0400

    Delete BotBase-main directory

--- BotBase-main/src/utils/handlers/event.js
+++
@@ -1,9 +0,0 @@
-const { readdirSync } = require('fs');
-
-module.exports = (client) => {
-	let events = readdirSync("./src/events/").filter(x => x.endsWith(".js")).map(x => x.split(".")[0]);
-	events.forEach(file => {
-		client.on(file, require(`../../events/${file}`).bind(null, client));
-		console.log(`Iniciado el evento ${file}`)
-	});
-};
